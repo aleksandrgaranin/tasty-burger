eeb2becf2c4ec3d1c7b1c1f42eb7da5191af0e1f
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2020-08-15 14:47:22 -0700

    Reusing the Backdrop in SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.module.css';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
@@ -6,15 +6,29 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
 
-const layout =(props)=>(
-    <Aux>
-        
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
 
-export default layout;
+class Layout extends Component{
+    state ={
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler =()=>{
+        this.setState({showSideDrawer: false});
+    }
+
+    render(){ 
+        return(
+            <Aux>                
+                <Toolbar/>
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+} 
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,11 +7,11 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar=(props)=>(
     <header className={classes.Toolbar}>
-        <div >Menu</div>
+        <div>Menu</div>
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>            
         </nav>
     </header>
