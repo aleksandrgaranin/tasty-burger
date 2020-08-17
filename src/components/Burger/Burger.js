9a7969b7f70f57e7e788e1f7370bec22124dc465
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2020-08-17 11:41:20 -0700

    changed OrderSummary and Modal to class component and removed unnecassary render components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,9 +8,12 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            return[...Array(props.ingredients[igKey])].map((_, i) => {
-               return <BurgerIngredient key={igKey + i} type={igKey}/>
-            });}).reduce((arr, el) => {return arr.concat(el)}, []);
+            return[...Array(props.ingredients[igKey])]
+                .map((_, i) => {
+                    return <BurgerIngredient 
+                        key={igKey + i} 
+                        type={igKey}/>
+                });}).reduce((arr, el) => {return arr.concat(el)}, []);
 
     console.log(transformedIngredients)
     if (transformedIngredients.length === 0) {
@@ -19,7 +22,7 @@ const burger = (props) => {
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            {transformedIngredients}
+                {transformedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
