512e4abaf4f94699e485c81cd2c3a7a725210b46
aleksandGG <aleksandrgaranin89@gmail.com>
2020-09-08 14:41:41 -0700

    Handling User Input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,6 +85,19 @@ class ContactData extends Component {
             });
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }; //not a deep clone just {orderForm:name, email ...}
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        }; //dipper clone of order form {orderForm:{name: props}, {email: props}, ...} but not all the way
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    }
+
     render () {
         const formElementsArrey = [];
         for (let key in this.state.orderForm){
@@ -102,7 +115,8 @@ class ContactData extends Component {
                             key={formElement.id}
                             elementType={formElement.config.elementType}
                             elementConfig={formElement.config.elementConfig}
-                            value={formElement.config.value}/>
+                            value={formElement.config.value}
+                            changed={(event)=> this.inputChangedHandler(event, formElement.id)}/>
                     ))}
                     <Button btnType="Success" clicked={this.orderHendler}>ORDER</Button>
                 </form>
